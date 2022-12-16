2687f6d25a84b96f1f186cf799ce3d71bf6999b0
Nikolay <labazopelastra@gmail.com>
2022-12-17 04:02:10 +0700

    add exam45

---
+++ Exam45/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Напишите программу, которая будет
+создавать копию заданного массива с помощью
+поэлементного копирования.*/
+
+int[] PrintArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0, 15);
+        System.Console.Write(" " + array[i] + " ");
+    }
+    return array;
+}
+
+int[] copyArray(int[] array)
+{
+    int[] copArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        copArray[i] = array[i];
+        System.Console.Write(" "+copArray[i]+" ");
+    }
+    return copArray;
+}
+
+int[] array = PrintArray(6);
+System.Console.WriteLine();
+int[] copArray=copyArray(array);
+
+
